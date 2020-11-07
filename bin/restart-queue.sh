142aeb30dd7072400412c4eae12840a0047da2ab
worgflores <worgflores@gmail.com>
2020-11-07 17:30:55 +0800

    removed

--- bin/restart-queue.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-RUN_DIR=`pwd`
-cd ../queue
-docker-compose down
-docker system prune -f
-sleep 1
-docker-compose up -d
-docker-compose logs -f queue-server
-cd $RUN_DIR
--- bin/stop-queue.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-RUN_DIR=`pwd`
-cd ../queue
-docker-compose down
-docker system prune -f
-cd $RUN_DIR

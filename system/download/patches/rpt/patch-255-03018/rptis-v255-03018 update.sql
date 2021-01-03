335268c00d8f3b513055d514c818bf8dfbffd755
worgflores <worgflores@gmail.com>
2021-01-03 15:23:06 +0800

    system: added

---
+++ system/download/patches/rpt/patch-255-03018/rptis-v255-03018 update.sql
@@ -0,0 +1,693 @@
+/* 255-03013 */
+alter table resection_item add newfaas_claimno varchar(25);
+alter table resection_item add faas_claimno varchar(25);
+
+
+
+/* 255-03015 */
+
+CREATE TABLE `rptcertification_online` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `reftype` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `refdate` date NOT NULL,
+  `orno` varchar(25) DEFAULT NULL,
+  `ordate` date DEFAULT NULL,
+  `oramount` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orno` (`orno`),
+  CONSTRAINT `fk_rptcertification_online_rptcertification` FOREIGN KEY (`objid`) REFERENCES `rptcertification` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+CREATE TABLE `assessmentnotice_online` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `reftype` varchar(25) NOT NULL,
+  `refid` varchar(50) NOT NULL,
+  `refno` varchar(50) NOT NULL,
+  `refdate` date NOT NULL,
+  `orno` varchar(25) DEFAULT NULL,
+  `ordate` date DEFAULT NULL,
+  `oramount` decimal(16,2) DEFAULT NULL,
+  PRIMARY KEY (`objid`),
+  KEY `ix_state` (`state`),
+  KEY `ix_refid` (`refid`),
+  KEY `ix_refno` (`refno`),
+  KEY `ix_orno` (`orno`),
+  CONSTRAINT `fk_assessmentnotice_online_assessmentnotice` FOREIGN KEY (`objid`) REFERENCES `assessmentnotice` (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+
+/*===============================================================
+**
+** FAAS ANNOTATION
+**
+===============================================================*/
+CREATE TABLE `faasannotation_faas` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `faas_objid` varchar(50) NOT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+alter table faasannotation_faas 
+add constraint fk_faasannotationfaas_faasannotation foreign key(parent_objid)
+references faasannotation (objid)
+;
+
+alter table faasannotation_faas 
+add constraint fk_faasannotationfaas_faas foreign key(faas_objid)
+references faas (objid)
+;
+
+create index ix_parent_objid on faasannotation_faas(parent_objid)
+;
+
+create index ix_faas_objid on faasannotation_faas(faas_objid)
+;
+
+
+create unique index ux_parent_faas on faasannotation_faas(parent_objid, faas_objid)
+;
+
+alter table faasannotation modify column faasid varchar(50) null
+;
+
+
+
+-- insert annotated faas
+insert into faasannotation_faas(
+  objid, 
+  parent_objid,
+  faas_objid 
+)
+select 
+  objid, 
+  objid as parent_objid,
+  faasid as faas_objid 
+from faasannotation
+;
+
+
+
+/*============================================
+*
+*  LEDGER FAAS FACTS
+*
+=============================================*/
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('rptledger_rule_include_ledger_faases', '0', 'Include Ledger FAASes as rule facts', 'checkbox', 'LANDTAX')
+;
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('rptledger_post_ledgerfaas_by_actualuse', '0', 'Post by Ledger FAAS by actual use', 'checkbox', 'LANDTAX')
+;
+
+
+
+/* 255-03016 */
+
+/*================================================================
+*
+* RPTLEDGER REDFLAG
+*
+================================================================*/
+
+CREATE TABLE `rptledger_redflag` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `caseno` varchar(25) NULL,
+  `dtfiled` datetime NULL,
+  `type` varchar(25) NOT NULL,
+  `finding` text,
+  `remarks` text,
+  `blockaction` varchar(25) DEFAULT NULL,
+  `filedby_objid` varchar(50) DEFAULT NULL,
+  `filedby_name` varchar(255) DEFAULT NULL,
+  `filedby_title` varchar(50) DEFAULT NULL,
+  `resolvedby_objid` varchar(50) DEFAULT NULL,
+  `resolvedby_name` varchar(255) DEFAULT NULL,
+  `resolvedby_title` varchar(50) DEFAULT NULL,
+  `dtresolved` datetime NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+create index ix_parent_objid on rptledger_redflag(parent_objid)
+;
+create index ix_state on rptledger_redflag(state)
+;
+create unique index ux_caseno on rptledger_redflag(caseno)
+;
+create index ix_type on rptledger_redflag(type)
+;
+create index ix_filedby_objid on rptledger_redflag(filedby_objid)
+;
+create index ix_resolvedby_objid on rptledger_redflag(resolvedby_objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_rptledger foreign key (parent_objid)
+references rptledger(objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_filedby foreign key (filedby_objid)
+references sys_user(objid)
+;
+
+alter table rptledger_redflag 
+add constraint fk_rptledger_redflag_resolvedby foreign key (resolvedby_objid)
+references sys_user(objid)
+;
+
+
+
+
+
+/*==================================================
+* RETURNED TASK 
+==================================================*/
+alter table faas_task add returnedby varchar(100)
+;
+alter table subdivision_task add returnedby varchar(100)
+;
+alter table consolidation_task add returnedby varchar(100)
+;
+alter table cancelledfaas_task add returnedby varchar(100)
+;
+alter table resection_task add returnedby varchar(100)
+;
+
+
+/* 255-03017 */
+
+/*================================================================
+*
+* LANDTAX SHARE POSTING
+*
+================================================================*/
+
+alter table rptpayment_share 
+	add iscommon int,
+	add `year` int
+;
+
+update rptpayment_share set iscommon = 0 where iscommon is null 
+;
+
+
+CREATE TABLE `cashreceipt_rpt_share_forposting` (
+  `objid` varchar(50) NOT NULL,
+  `receiptid` varchar(50) NOT NULL,
+  `rptledgerid` varchar(50) NOT NULL,
+  `txndate` datetime NOT NULL,
+  `error` int(255) NOT NULL,
+  `msg` text,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+create UNIQUE index `ux_receiptid_rptledgerid` on cashreceipt_rpt_share_forposting (`receiptid`,`rptledgerid`)
+;
+create index `fk_cashreceipt_rpt_share_forposing_rptledger` on cashreceipt_rpt_share_forposting (`rptledgerid`)
+;
+create index `fk_cashreceipt_rpt_share_forposing_cashreceipt` on cashreceipt_rpt_share_forposting (`receiptid`)
+;
+
+alter table cashreceipt_rpt_share_forposting add CONSTRAINT `fk_cashreceipt_rpt_share_forposing_rptledger` 
+FOREIGN KEY (`rptledgerid`) REFERENCES `rptledger` (`objid`)
+;
+alter table cashreceipt_rpt_share_forposting add CONSTRAINT `fk_cashreceipt_rpt_share_forposing_cashreceipt` 
+FOREIGN KEY (`receiptid`) REFERENCES `cashreceipt` (`objid`)
+;
+
+
+
+
+/*==================================================
+**
+** BLDG DATE CONSTRUCTED SUPPORT 
+**
+===================================================*/
+
+alter table bldgrpu add dtconstructed date;
+
+
+
+/* 255-03018 */
+
+/*==================================================
+**
+** ONLINE BATCH GR 
+**
+===================================================*/
+drop table if exists zz_tmp_batchgr_item 
+;
+drop table if exists zz_tmp_batchgr
+;
+
+create table zz_tmp_batchgr 
+select * from batchgr
+;
+
+create table zz_tmp_batchgr_item 
+select * from batchgr_item
+;
+
+drop table if exists batchgr_task
+;
+
+alter table batchgr 
+  add txntype_objid varchar(50),
+  add txnno varchar(25),
+  add txndate datetime,
+  add effectivityyear int,
+  add effectivityqtr int,
+  add originlgu_objid varchar(50)
+;
+
+
+create index ix_ry on batchgr(ry)
+;
+create index ix_txnno on batchgr(txnno)
+;
+create index ix_classificationid on batchgr(classification_objid)
+;
+create index ix_section on batchgr(section)
+;
+
+alter table batchgr 
+add constraint fk_batchgr_lguid foreign key(lgu_objid) 
+references sys_org(objid)
+;
+
+alter table batchgr 
+add constraint fk_batchgr_barangayid foreign key(barangay_objid) 
+references sys_org(objid)
+;
+
+alter table batchgr 
+add constraint fk_batchgr_classificationid foreign key(classification_objid) 
+references propertyclassification(objid)
+;
+
+
+alter table batchgr_item add subsuffix int
+;
+
+alter table batchgr_item 
+add constraint fk_batchgr_item_faas foreign key(objid) 
+references faas(objid)
+;
+
+create table `batchgr_task` (
+  `objid` varchar(50) not null,
+  `refid` varchar(50) default null,
+  `parentprocessid` varchar(50) default null,
+  `state` varchar(50) default null,
+  `startdate` datetime default null,
+  `enddate` datetime default null,
+  `assignee_objid` varchar(50) default null,
+  `assignee_name` varchar(100) default null,
+  `assignee_title` varchar(80) default null,
+  `actor_objid` varchar(50) default null,
+  `actor_name` varchar(100) default null,
+  `actor_title` varchar(80) default null,
+  `message` varchar(255) default null,
+  `signature` longtext,
+  `returnedby` varchar(100) default null,
+  primary key (`objid`),
+  key `ix_assignee_objid` (`assignee_objid`),
+  key `ix_refid` (`refid`)
+) engine=innodb default charset=utf8;
+
+alter table batchgr_task 
+add constraint fk_batchgr_task_batchgr foreign key(refid) 
+references batchgr(objid)
+;
+
+
+
+
+drop view if exists vw_batchgr
+;
+
+create view vw_batchgr 
+as 
+select 
+  bg.*,
+  l.name as lgu_name,
+  b.name as barangay_name,
+  pc.name as classification_name,
+  t.objid AS taskid,
+  t.state AS taskstate,
+  t.assignee_objid 
+from batchgr bg
+inner join sys_org l on bg.lgu_objid = l.objid 
+left join sys_org b on bg.barangay_objid = b.objid
+left join propertyclassification pc on bg.classification_objid = pc.objid 
+left join batchgr_task t on bg.objid = t.refid  and t.enddate is null 
+;
+
+
+
+
+/*===========================================
+*
+*  ENTITY MAPPING (PROVINCE)
+*
+============================================*/
+
+DROP TABLE IF EXISTS `entity_mapping`
+;
+
+CREATE TABLE `entity_mapping` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `org_objid` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+drop view if exists vw_entity_mapping
+;
+
+create view vw_entity_mapping
+as 
+select 
+  r.*,
+  e.entityno,
+  e.name, 
+  e.address_text as address_text,
+  a.province as address_province,
+  a.municipality as address_municipality
+from entity_mapping r 
+inner join entity e on r.objid = e.objid 
+left join entity_address a on e.address_objid = a.objid
+left join sys_org b on a.barangay_objid = b.objid 
+left join sys_org m on b.parent_objid = m.objid 
+;
+
+
+
+
+/*===========================================
+*
+*  CERTIFICATION UPDATES
+*
+============================================*/
+drop view if exists vw_rptcertification_item
+;
+
+create view vw_rptcertification_item
+as 
+SELECT 
+  rci.rptcertificationid,
+  f.objid as faasid,
+  f.fullpin, 
+  f.tdno,
+  e.objid as taxpayerid,
+  e.name as taxpayer_name, 
+  f.owner_name, 
+  f.administrator_name,
+  f.titleno,  
+  f.rpuid, 
+  pc.code AS classcode, 
+  pc.name AS classname,
+  so.name AS lguname,
+  b.name AS barangay, 
+  r.rputype, 
+  r.suffix,
+  r.totalareaha AS totalareaha,
+  r.totalareasqm AS totalareasqm,
+  r.totalav,
+  r.totalmv, 
+  rp.street,
+  rp.blockno,
+  rp.cadastrallotno,
+  rp.surveyno,
+  r.taxable,
+  f.effectivityyear,
+  f.effectivityqtr
+FROM rptcertificationitem rci 
+  INNER JOIN faas f ON rci.refid = f.objid 
+  INNER JOIN rpu r ON f.rpuid = r.objid 
+  INNER JOIN propertyclassification pc ON r.classification_objid = pc.objid 
+  INNER JOIN realproperty rp ON f.realpropertyid = rp.objid 
+  INNER JOIN barangay b ON rp.barangayid = b.objid 
+  INNER JOIN sys_org so on f.lguid = so.objid 
+  INNER JOIN entity e on f.taxpayer_objid = e.objid 
+;
+
+
+
+/*===========================================
+*
+*  SUBDIVISION ASSISTANCE
+*
+============================================*/
+drop table if exists subdivision_assist_item
+; 
+
+drop table if exists subdivision_assist
+; 
+
+CREATE TABLE `subdivision_assist` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `taskstate` varchar(50) NOT NULL,
+  `assignee_objid` varchar(50) NOT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+alter table subdivision_assist 
+add constraint fk_subdivision_assist_subdivision foreign key(parent_objid)
+references subdivision(objid)
+;
+
+alter table subdivision_assist 
+add constraint fk_subdivision_assist_user foreign key(assignee_objid)
+references sys_user(objid)
+;
+
+create index ix_parent_objid on subdivision_assist(parent_objid)
+;
+
+create index ix_assignee_objid on subdivision_assist(assignee_objid)
+;
+
+create unique index ux_parent_assignee on subdivision_assist(parent_objid, taskstate, assignee_objid)
+;
+
+
+CREATE TABLE `subdivision_assist_item` (
+`objid` varchar(50) NOT NULL,
+  `subdivision_objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `pintype` varchar(10) NOT NULL,
+  `section` varchar(5) NOT NULL,
+  `startparcel` int(255) NOT NULL,
+  `endparcel` int(255) NOT NULL,
+  `parcelcount` int(11) DEFAULT NULL,
+  `parcelcreated` int(11) DEFAULT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+alter table subdivision_assist_item 
+add constraint fk_subdivision_assist_item_subdivision foreign key(subdivision_objid)
+references subdivision(objid)
+;
+
+alter table subdivision_assist_item 
+add constraint fk_subdivision_assist_item_subdivision_assist foreign key(parent_objid)
+references subdivision_assist(objid)
+;
+
+create index ix_subdivision_objid on subdivision_assist_item(subdivision_objid)
+;
+
+create index ix_parent_objid on subdivision_assist_item(parent_objid)
+;
+
+
+
+/*==================================================
+**
+** REALTY TAX CREDIT
+**
+===================================================*/
+
+drop table if exists rpttaxcredit
+;
+
+
+
+CREATE TABLE `rpttaxcredit` (
+  `objid` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `txnno` varchar(25) DEFAULT NULL,
+  `txndate` datetime DEFAULT NULL,
+  `reftype` varchar(25) DEFAULT NULL,
+  `refid` varchar(50) DEFAULT NULL,
+  `refno` varchar(25) NOT NULL,
+  `refdate` date NOT NULL,
+  `amount` decimal(16,2) NOT NULL,
+  `amtapplied` decimal(16,2) NOT NULL,
+  `rptledger_objid` varchar(50) NOT NULL,
+  `srcledger_objid` varchar(50) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `approvedby_objid` varchar(50) DEFAULT NULL,
+  `approvedby_name` varchar(150) DEFAULT NULL,
+  `approvedby_title` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+
+create index ix_state on rpttaxcredit(state)
+;
+
+create index ix_type on rpttaxcredit(type)
+;
+
+create unique index ux_txnno on rpttaxcredit(txnno)
+;
+
+create index ix_reftype on rpttaxcredit(reftype)
+;
+
+create index ix_refid on rpttaxcredit(refid)
+;
+
+create index ix_refno on rpttaxcredit(refno)
+;
+
+create index ix_rptledger_objid on rpttaxcredit(rptledger_objid)
+;
+
+create index ix_srcledger_objid on rpttaxcredit(srcledger_objid)
+;
+
+alter table rpttaxcredit
+add constraint fk_rpttaxcredit_rptledger foreign key (rptledger_objid)
+references rptledger (objid)
+;
+
+alter table rpttaxcredit
+add constraint fk_rpttaxcredit_srcledger foreign key (srcledger_objid)
+references rptledger (objid)
+;
+
+alter table rpttaxcredit
+add constraint fk_rpttaxcredit_sys_user foreign key (approvedby_objid)
+references sys_user(objid)
+;
+
+
+
+
+
+/*==================================================
+**
+** MACHINE SMV
+**
+===================================================*/
+
+CREATE TABLE `machine_smv` (
+  `objid` varchar(50) NOT NULL,
+  `parent_objid` varchar(50) NOT NULL,
+  `machine_objid` varchar(50) NOT NULL,
+  `expr` varchar(255) NOT NULL,
+  `previd` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`objid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
+create index ix_parent_objid on machine_smv(parent_objid)
+;
+create index ix_machine_objid on machine_smv(machine_objid)
+;
+create index ix_previd on machine_smv(previd)
+;
+create unique index ux_parent_machine on machine_smv(parent_objid, machine_objid)
+;
+
+
+
+alter table machine_smv
+add constraint fk_machinesmv_machrysetting foreign key (parent_objid)
+references machrysetting (objid)
+;
+
+alter table machine_smv
+add constraint fk_machinesmv_machine foreign key (machine_objid)
+references machine(objid)
+;
+
+
+alter table machine_smv
+add constraint fk_machinesmv_machinesmv foreign key (previd)
+references machine_smv(objid)
+;
+
+
+create view vw_machine_smv 
+as 
+select 
+  ms.*, 
+  m.code,
+  m.name
+from machine_smv ms 
+inner join machine m on ms.machine_objid = m.objid 
+;
+
+alter table machdetail 
+  add smvid varchar(50),
+  add params text
+;
+
+update machdetail set params = '[]' where params is null
+;
+
+create index ix_smvid on machdetail(smvid)
+;
+
+
+alter table machdetail 
+add constraint fk_machdetail_machine_smv foreign key(smvid)
+references machine_smv(objid)
+;
+
+
+
+
+/*==================================================
+**
+** AFFECTED FAS TXNTYPE (DP)
+**
+===================================================*/
+
+INSERT INTO `sys_var` (`name`, `value`, `description`, `datatype`, `category`) 
+VALUES ('faas_affected_rpu_txntype_dp', '0', 'Set affected improvements FAAS txntype to DP e.g. SD and CS', 'checkbox', 'ASSESSOR')
+;
+
+
+
